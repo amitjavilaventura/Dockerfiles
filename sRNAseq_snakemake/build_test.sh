c2252e0801a5703ead8e45d00dfc67127a21f896
amitjavilaventura <adria.mitjavila94@gmail.com>
2021-08-12 14:26:37 +0200

    update

--- sRNAseq_snakemake/build_test.sh
+++ sRNAseq_snakemake/build_test.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 IMAGE_VERSION=$(date '+%d%m%y')
-docker build -t amitjavilaventura/sRNAseq_snakemake:${IMAGE_VERSION} -t amitjavilaventura/sRNAseq_snakemake:latest . && \
-docker push amitjavilaventura/sRNAseq_snakemake:${IMAGE_VERSION}
-docker push amitjavilaventura/sRNAseq_snakemake:latest
+docker build -t amitjavilaventura/srnaseq_snakemake:${IMAGE_VERSION} -t amitjavilaventura/srnaseq_snakemake:latest . && \
+docker push amitjavilaventura/srnaseq_snakemake:${IMAGE_VERSION}
+docker push amitjavilaventura/srnaseq_snakemake:latest
