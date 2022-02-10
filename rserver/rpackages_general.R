485df8c1ed0ea01e95694e8b79383cd4b8ec73da
amitjavilaventura <adria.mitjavila94@gmail.com>
2022-02-10 12:49:37 +0100

    add ensembl_compara, repeatmasker and manna to srnaseq

---
+++ rserver/rpackages_general.R
@@ -0,0 +1,40 @@
+
+# Install packages function -- function taken from dfernandezperez
+install_packages <- function(packages) {
+  for ( i in packages ) {
+    if ( grepl(i, pattern = '/') ) {
+      if ( grepl(i, pattern = '@') ) {
+        name <- strsplit(i, split = '/')[[1]][2]
+        name <- strsplit(name, split = '@')[[1]][1]
+      } else {
+        name <- strsplit(i, split = '/')[[1]][2]
+      }
+    } else {
+      name <- i
+    }
+    if ( (name %in% library()$results[,1] ) == FALSE ) {
+      message(paste0('[', format(Sys.time(), '%Y-%m-%d %H:%M:%S'), "] '", i, "' will be installed."))
+      BiocManager::install(i, update = TRUE, ask = FALSE, quiet = TRUE, Ncpus = 2)
+      if ( (name %in% library()$results[,1] ) == FALSE ) {
+        message(paste0('[', format(Sys.time(), '%Y-%m-%d %H:%M:%S'), "] '", i, "' installation failed."))
+        stop()
+      } else {
+        message(paste0('[', format(Sys.time(), '%Y-%m-%d %H:%M:%S'), "] '", i, "' successfully installed."))
+      }
+    } else {
+      message(paste0('[', format(Sys.time(), '%Y-%m-%d %H:%M:%S'), "] '", i, "' already installed."))
+    }
+  }
+}
+
+
+# define packages to install from cran or bioconductor
+packages_utilities      <- c("devtools", "here", "workflowr", "reticulate", "openxlsx", "stringi", 
+                             "plyr", "DT", "data.table", "tidyverse", "openxlsx", "magrittr", "reshape2")
+packages_plots          <- c("ggforce", "ggpubr", "RColorBrewer", "scales", "circlize", "cowplot", "ggdendro", "ggExtra",
+                             "patchwork", "ggthemes", "pheatmap", "ComplexHeatmap", "ggvenn", "ggrepel", "ggrastr",
+                             "ggbeeswarm", "ggbio", "ggExtra", "thomasp85/ggfx", "ggsignif", "UpSetR", "viridis", "wesanderson")
+
+# Install packages from bioconductor
+install_packages(packages_utilities)
+install_packages(packages_plots)
